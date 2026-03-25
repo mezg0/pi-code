dbe89eba8214dca94b8083e94f0676fed72209f8
Brandon Gomes <brandongomes1705@gmail.com>
2026-03-25 11:58:29 +0000

    Update message.tsx

--- src/renderer/src/components/ai-elements/message.tsx
+++ src/renderer/src/components/ai-elements/message.tsx
@@ -330,6 +330,7 @@ export const MessageResponse = memo(
         "size-full [&>*:first-child]:mt-0 [&>*:last-child]:mb-0",
         className
       )}
+      linkSafety={false}
       plugins={streamdownPlugins}
       {...props}
     />
